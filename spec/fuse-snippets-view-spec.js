9ca052000051edf73ca082d01cf6f4ca66e61ecd
Louis G. Vichy <lgvichy@gmail.com>
2016-08-13 05:10:49 -0400

    Initialized fuse-snippets for FuseTool's SDK

---
+++ spec/fuse-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FuseSnippetsView from '../lib/fuse-snippets-view';
+
+describe('FuseSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
